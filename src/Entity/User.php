3fb8cf292a2bfd6baed5a4b8dede40efb2e81f2a
Leosbel Poll Sotomayor <leitopoll25@gmail.com>
2019-02-09 11:46:51 -0300

    Modify some things like    hide toolbar symfony in dev (server is difiult to clean cache prod)    make the password more secure    some colors    some icons    some validators translations files    some styles

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,18 +4,35 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use FOS\UserBundle\Model\User as BaseUser;
+use Symfony\Component\Validator\Constraints as Assert;  
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
 class User extends BaseUser
 {
-    /**
-     * @ORM\Id
-     * @ORM\Column(type="uuid", unique=true)
-     * @ORM\GeneratedValue(strategy="CUSTOM")
-     * @ORM\CustomIdGenerator(class="Ramsey\Uuid\Doctrine\UuidGenerator")
-     */
+        /**
+         * @ORM\Id
+         * @ORM\Column(type="uuid", unique=true)
+         * @ORM\GeneratedValue(strategy="CUSTOM")
+         * @ORM\CustomIdGenerator(class="Ramsey\Uuid\Doctrine\UuidGenerator")
+         */
     protected $id;
+
+    /**
+     * @Assert\Length(
+     *     min=6,
+     *     max=19,
+     *     minMessage="user.password.short",
+     *     groups={"Profile", "ResetPassword", "Registration", "ChangePassword"}
+     * )
+     * @Assert\Regex(
+     *     pattern="/^(?=.*[A-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^*(),.?+-])\S{9,16}$/",
+     *     message="user.password.difficulty",
+     *     groups={"Profile", "ResetPassword", "Registration", "ChangePassword"}
+     * )
+     */
+    
+    protected $plainPassword;
 
 }
